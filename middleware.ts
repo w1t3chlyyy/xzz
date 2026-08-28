3d1b57008fbc851f4d9147c77be88b2147b36d65
w1t3chlyyy <w1t3chlyyy@mail.ru>
2026-08-28 19:10:22 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
 // middleware.ts
+// Импортируем из 'next/server' только то, что поддерживается в Edge
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  // Простой middleware без Supabase
+  // Простой middleware
   return NextResponse.next();
 }
 
